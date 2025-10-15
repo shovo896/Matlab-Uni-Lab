24fbec87c0f639f6609d4cd5bbb9993ab80b1866
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-10-15 09:49:52 +0600

    Create myfunction.m

---
+++ myfunction.m
@@ -0,0 +1,12 @@
+function result = myfunction(x,y)
+     sumval=add(x,y);
+     prodval=multiply(x,y);
+     result=sumval+prodval ;
+end 
+
+function s = add(a,b)
+        s=a+b ;
+end 
+function p=multiply(a,b)
+        p=a*b ;
+end 
